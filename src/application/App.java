6688de459aa791b05770b9693dc0012500266509
earl-stephens <stephensearl@hotmail.com>
2021-12-25 23:09:02 -0700

    Complete coding challenge

--- src/application/App.java
+++ src/application/App.java
@@ -13,6 +13,15 @@ public class App {
 		
 		int added = calc.add(7, 5);
 		System.out.println("7 + 5 = " + added);
+		
+		int difference = calc.subtract(8, 3);
+		System.out.println("8 - 3 = " + difference);
+
+		int quotient = calc.divide(27, 9);
+		System.out.println("27 / 9 = " + quotient);
+		
+		int product = calc.multiply(6, 7);
+		System.out.println("6 * 7 = " + product);
 	}
 
 }
